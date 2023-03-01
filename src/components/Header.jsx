8d8c5d2ab69ac610bc8472900abc85ec090efcd0
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-03-01 13:38:05 -0300

    -- Header Pc --

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,51 +40,54 @@ const Header = () => {
             H o l a ! S o y <span>José Alvarez Martinez</span>D e s a r r o l l
             a d o r &nbsp; F r o n t &nbsp; E n d
           </h1>
+          <div className="botones">
+            <div className="centrar-boton">
+              <a
+                target={"_blank"}
+                href="https://drive.google.com/file/d/1HhbV_plbRYvR8BhxZeJX4aeGwx1_xYY6/view?usp=sharing"
+                className="descargar-cv"
+              >
+                Descargar CV
+              </a>
+            </div>
+            <div className="redes-sociales">
+              <div className="red-social-fondo">
+                <a
+                  target={"_blank"}
+                  href="https://www.linkedin.com/in/jos%C3%A9-alvarez-martinez-621404211/"
+                >
+                  <CiLinkedin color="#fff" size={"2.5rem"} />
+                </a>
+              </div>
+              <div className="red-social-fondo ">
+                <a
+                  target={"_blank"}
+                  href="https://github.com/JoseAlvarezMartinez"
+                >
+                  <BsGithub color="#fff" size={"2.5rem"} />
+                </a>
+              </div>
+            </div>
+          </div>
+          </div>
           <div className="centrar-boton">
-            <a
-              target={"_blank"}
-              href="https://drive.google.com/file/d/1HhbV_plbRYvR8BhxZeJX4aeGwx1_xYY6/view?usp=sharing"
-              className="descargar-cv"
+            <svg
+              className={`tienda-online-arrow ${isOpen ? "z-1" : ""}`}
+              width="14"
+              height="16"
+              viewBox="0 0 14 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              Descargar CV
-            </a>
-          </div>
-          <div className="redes-sociales">
-            <div className="red-social-fondo">
-              <a
-                target={"_blank"}
-                href="https://www.linkedin.com/in/jos%C3%A9-alvarez-martinez-621404211/"
-              >
-                <CiLinkedin color="#fff" size={"2.5rem"} />
-              </a>
-            </div>
-            <div className="red-social-fondo ">
-              <a
-                target={"_blank"}
-                href="https://github.com/JoseAlvarezMartinez"
-              >
-                <BsGithub color="#fff" size={"2.5rem"} />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="centrar-boton">
-          <svg
-            className={`tienda-online-arrow ${isOpen ? "z-1" : ""}`}
-            width="14"
-            height="16"
-            viewBox="0 0 14 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1.16602 8.83337L6.99935 14.6667L12.8327 8.83337M6.99935 14.6667V1.33337V14.6667Z"
-              stroke="white"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></path>
-          </svg>
+              <path
+                d="M1.16602 8.83337L6.99935 14.6667L12.8327 8.83337M6.99935 14.6667V1.33337V14.6667Z"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>
+            </svg>
+       
         </div>
       </div>
     </header>
