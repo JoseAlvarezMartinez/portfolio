d9a7bd03b4c14c8d1ff900752a1b8b037938bbdd
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-03-03 14:44:15 -0300

    -- Imagenes adaptadas --

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,14 +1,15 @@
-import LinkedIn from "../assets/linkedin.png"
-import Mail from "../assets/mail.png"
-import WhatsApp from "../assets/wpplogo.webp"
+import LinkedIn from "../assets/linkedin.png";
+import Mail from "../assets/mail.png";
+import WhatsApp from "../assets/wpplogo.webp";
 
 const Contacto = () => {
   return (
     <footer>
       <h2 className="titulo-h2">Contacto</h2>
-      <img className="linkedinLogo" src={LinkedIn} alt="" />
-      <img className="MailLogo"  src={Mail} alt="" />
-      <img className="WppLogo"  src={WhatsApp} alt="" />
+
+      {/* <img className="linkedinLogo" src={LinkedIn} alt="" />
+      <img className="mailLogo"  src={Mail} alt="" />
+      <img className="wppLogo"  src={WhatsApp} alt="" /> */}
     </footer>
   );
 };
